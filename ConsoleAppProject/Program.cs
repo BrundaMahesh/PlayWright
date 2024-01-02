14274d342d33001dbb14aad112ebfa29b0d280ac
brunda8704 <brunda8704@gmail.com>
2024-01-02 10:28:38 +0530

    Add project files.

---
+++ ConsoleAppProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Playwright;
+
+//playwright startup
+using var playwright = await Playwright.CreateAsync();
+
+//launch browser
+await using var browser = await playwright.Chromium.LaunchAsync();
+
+//page instance
+var context = await browser.NewContextAsync();
+var page = await context.NewPageAsync();
+
+
+
