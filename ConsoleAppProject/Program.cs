0909a0837f2623c1480ac8d963ba4b6ec9a8ca75
brunda8704 <brunda8704@gmail.com>
2024-01-02 14:49:46 +0530

    commit

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -10,5 +10,11 @@ await using var browser = await playwright.Chromium.LaunchAsync();
 var context = await browser.NewContextAsync();
 var page = await context.NewPageAsync();
 
+Console.WriteLine("Opened Browser");
+await page.GotoAsync("https://www.google.com");
+Console.WriteLine("Page Loaded");
 
+await page.GetByTitle("Search").FillAsync("hp laptop");
+Console.WriteLine("Typed Search text");
 
+//await page.GetByRole("button").ClickAsync();
