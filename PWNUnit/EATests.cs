d20559c7cb08bd21a9dc9590569afbcd22faf231
BrundaMahesh <brunda.mahesh@ust.com>
2024-01-03 12:33:36 +0530

    modified

--- PWNUnit/EATests.cs
+++ PWNUnit/EATests.cs
@@ -12,26 +12,45 @@ namespace PWNUnit
     [TestFixture]
     internal class EATests : PageTest
     {
-        [Test]
-        public async Task LoginTest()
+        [SetUp]
+        public async Task SetUp()
         {
             Console.WriteLine("Opened Browser");
             await Page.GotoAsync("http://eaapp.somee.com/");
-            Console.WriteLine("Page Loaded");
+            Console.WriteLine("eaapp.somee home page loaded");
+        }
 
-            await Page.GetByText("Login").ClickAsync();
+        [Test]
+        public async Task LoginTest()
+        {
+
+            //await Page.GetByText("Login").ClickAsync();
+
+            /*ILocator loginLink = Page.Locator(selector: "text=Login");
+            await loginLink.ClickAsync();
+            */
+
+            await Page.ClickAsync(selector: "text=Login");
+
             await Console.Out.WriteLineAsync("Clicked on Login link");
 
-            //full url
+
+            //full url assert
             await Expect(Page).ToHaveURLAsync("http://eaapp.somee.com/Account/Login");
 
-            //partial url
+            //partial url assert
             //await Expect(Page).ToHaveURLAsync(new Regex("Login"));
 
-            await Page.GetByLabel("UserName").FillAsync(value: "admin");
-            await Console.Out.WriteLineAsync("Typed UserName");
 
+            /*await Page.GetByLabel("UserName").FillAsync(value: "admin");
+            await Console.Out.WriteLineAsync("Typed UserName");
             await Page.GetByLabel("Password").FillAsync(value: "password");
+            await Console.Out.WriteLineAsync("Typed Password");
+            */
+
+            await Page.FillAsync(selector: "#UserName", "admin");
+            await Console.Out.WriteLineAsync("Typed UserName");
+            await Page.FillAsync(selector: "#Password", "password");
             await Console.Out.WriteLineAsync("Typed Password");
 
             //await Page.Locator("//input[@value='Log in']").ClickAsync();
@@ -44,8 +63,11 @@ namespace PWNUnit
 
             await Expect(Page).ToHaveTitleAsync("Home - Execute Automation Employee App");
 
+            /*await Expect(Page.Locator(selector: "text='Hello admin!'")).ToBeVisibleAsync();
+            await Console.Out.WriteLineAsync("Hello admin! is visible");
+            */
 
-
+            await Expect(Page.Locator(selector: "text='Hello admin!'")).ToBeVisibleAsync();
         }
     }
 }
