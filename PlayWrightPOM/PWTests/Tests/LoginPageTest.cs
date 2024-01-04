d4623b51a5794ce72189a2d49beeba71fb8d65da
brunda8704 <brunda8704@gmail.com>
2024-01-04 10:56:05 +0530

    commit

--- PlayWrightPOM/PWTests/Tests/LoginPageTest.cs
+++ PlayWrightPOM/PWTests/Tests/LoginPageTest.cs
@@ -1,16 +1,41 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using PWPOM.PWTests.Pages;
+
 namespace PWPOM.PWTests.Tests
 {
-    public class LoginPageTest
+    [TestFixture]
+    public class LoginPageTest : PageTest
     {
         [SetUp]
-        public void Setup()
+        public async Task Setup()
         {
+            Console.WriteLine("Opened Browser");
+            await Page.GotoAsync("http://eaapp.somee.com/", new PageGotoOptions
+            {
+                Timeout = 5000,
+                WaitUntil = WaitUntilState.DOMContentLoaded
+            });
+            Console.WriteLine("eaapp.somee home page loaded");
         }
 
         [Test]
-        public void Test1()
+        [TestCase("admin", "password")]
+        [TestCase("admin", "****")]
+        public async Task LoginTest(string username, string password)
         {
-            Assert.Pass();
+            LoginPage loginPage = new LoginPage(Page);
+
+            await loginPage.ClickLoginLink();
+            await Console.Out.WriteLineAsync("Clicked on Login link");
+
+            await loginPage.Login(username,password);
+            await Console.Out.WriteLineAsync("Typed UserName");
+            await Console.Out.WriteLineAsync("Typed Password");
+            await Console.Out.WriteLineAsync("Clicked on Login button");
+
+            Assert.IsTrue(await loginPage.CheckhelloadminMessage());
+            await Console.Out.WriteLineAsync("Hello admin! is visible");
         }
     }
 }
