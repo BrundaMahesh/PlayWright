1e0d7f62fb12872ce624fd76cbd0161818ab3720
brunda8704 <brunda8704@gmail.com>
2024-01-04 12:35:01 +0530

    added new folders

--- PlayWrightPOM/PWTests/Tests/LoginPageTest.cs
+++ PlayWrightPOM/PWTests/Tests/LoginPageTest.cs
@@ -7,15 +7,31 @@ namespace PWPOM.PWTests.Tests
     [TestFixture]
     public class LoginPageTest : PageTest
     {
+        Dictionary<string, string> Properties;
+        private void ReadConfigSettings()
+        {
+            Properties = new Dictionary<string, string>();
+            string? currdir = Directory.GetParent(@"../../../")?.FullName;
+
+            string fileName = currdir + "/configsettings/config.properties";
+            string[] lines = File.ReadAllLines(fileName);
+            foreach (string line in lines)
+            {
+                if (!string.IsNullOrWhiteSpace(line) && line.Contains('='))
+                {
+                    string[] parts = line.Split('=');
+                    string key = parts[0].Trim();
+                    string value = parts[1].Trim();
+                    Properties[key] = value;
+                }
+            }
+        }
+
         [SetUp]
         public async Task Setup()
         {
             Console.WriteLine("Opened Browser");
-            await Page.GotoAsync("http://eaapp.somee.com/", new PageGotoOptions
-            {
-                Timeout = 5000,
-                WaitUntil = WaitUntilState.DOMContentLoaded
-            });
+            await Page.GotoAsync(Properties["baseUrl"]);
             Console.WriteLine("eaapp.somee home page loaded");
         }
 
@@ -24,6 +40,7 @@ namespace PWPOM.PWTests.Tests
         [TestCase("admin", "****")]
         public async Task LoginTest(string username, string password)
         {
+            /*
             LoginPage loginPage = new LoginPage(Page);
 
             await loginPage.ClickLoginLink();
@@ -35,6 +52,20 @@ namespace PWPOM.PWTests.Tests
             await Console.Out.WriteLineAsync("Clicked on Login button");
 
             Assert.IsTrue(await loginPage.CheckhelloadminMessage());
+            await Console.Out.WriteLineAsync("Hello admin! is visible");
+            */
+
+            NewLoginPage newLoginPage = new NewLoginPage(Page);
+
+            await newLoginPage.ClickLoginLink();
+            await Console.Out.WriteLineAsync("Clicked on Login link");
+
+            await newLoginPage.Login(username, password);
+            await Console.Out.WriteLineAsync("Typed UserName");
+            await Console.Out.WriteLineAsync("Typed Password");
+            await Console.Out.WriteLineAsync("Clicked on Login button");
+
+            Assert.IsTrue(await newLoginPage.CheckhelloadminMessage());
             await Console.Out.WriteLineAsync("Hello admin! is visible");
         }
     }
